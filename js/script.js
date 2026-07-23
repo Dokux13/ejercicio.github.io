6d5a45aa4eb58d19e673bb755296fae8a6a27e06
Dokux13 <d.espinoza.ig@gmail.com>
2026-07-23 13:24:18 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -63,7 +63,7 @@ fetch('data/puntos.geojson')
 
     let biomasa = Number(feature.properties.Total_biom) || 100;
 
-    let radio = Math.sqrt(biomasa) / 15;
+    let radio = Math.sqrt(biomasa) / 5;
 
 
     return L.circleMarker(latlng, {
